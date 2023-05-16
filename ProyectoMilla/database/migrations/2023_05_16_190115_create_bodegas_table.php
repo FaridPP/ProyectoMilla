44b7794f99e98d5f1a6928a7758c1cc814cd4fda
Farid <fariparedez@gmail.com>
2023-05-16 16:20:16 -0600

    Segundo commit

--- ProyectoMilla/database/migrations/2023_05_16_190115_create_bodegas_table.php
+++ ProyectoMilla/database/migrations/2023_05_16_190115_create_bodegas_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('bodegas', function (Blueprint $table) {
+            $table->id('codigo');
+            $table->string('nombre',100);
+            $table->string('ubicacion',100);
         });
     }
 
@@ -31,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bodegas');
     }
 };
